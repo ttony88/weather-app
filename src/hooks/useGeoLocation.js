c11fcb472a952002be9729a4e9de0fa4ad91df65
ttony62 <1988anton62@gmail.com>
2024-09-21 15:00:18 +0300

    fixed useGeoLocation

--- src/hooks/useGeoLocation.js
+++ src/hooks/useGeoLocation.js
@@ -4,14 +4,12 @@ export const useGeoLocation = () => {
     const [lat, setLat] = useState(0)
     const [long, setLong] = useState(0)
 
-    useEffect(() => {
+    function geo_success(position) {
+      setLat(position.coords.latitude)
+      setLong(position.coords.longitude)
+    }
 
-      function geo_success(position) {
-        
-        setLat(position.coords.latitude)
-        setLong(position.coords.longitude)
-      }
-    
+    useEffect(() => {
       navigator.geolocation.watchPosition(geo_success)
     }, [lat, long])
 
