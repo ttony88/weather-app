b3827ebc6149fef06075230a3b758202da4156ca
ttony62 <1988anton62@gmail.com>
2024-09-27 21:52:45 +0300

    added switching cities

--- src/App/HomeScreen/Header/SearchCity/SearchCity.jsx
+++ src/App/HomeScreen/Header/SearchCity/SearchCity.jsx
@@ -1,12 +1,33 @@
 import styles from './SearchCity.module.css'
 import searchImg from '../../../../img/search.svg'
+import { useState } from 'react'
+import { useGetCitysQuery, useGetForecastWeatherDataQuery } from '../../../../api/apiWeather'
 
-const SearchCity = () => {
+const SearchCity = ({ changingCoordinates }) => {
+
+    const [cityName, setCityName] = useState('')
+
+    const [isSearch, setIsSearch] = useState(false)
+
+    const { data: dataCity } = useGetCitysQuery(cityName)
+    
+    const onChangeHandler = (e) => {
+        setCityName(e.target.value)
+    }
+
+    const onClickHandler = () => {
+        setIsSearch(true)
+    }
 
     return(
         <div className={styles.container}>
-            <input className={styles.input} type="text" placeholder="Seatch..." />
-            <button className={styles.button}>
+            {isSearch && <input className={styles.input} 
+                   value={cityName} 
+                   onChange={onChangeHandler} 
+                   type="text" 
+                   placeholder="Seatch..." />}
+            {dataCity && dataCity.map(i => i && <div onClick={() => changingCoordinates(i.lat, i.lon)} key={i.id}>{`${i.name}, ${i.region}, ${i.country}`}</div>)}
+            <button onClick={onClickHandler} className={styles.button}>
                 <img src={searchImg} alt="search" />
             </button>
         </div>
