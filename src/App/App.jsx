59b72417150513dcc14c2fa4c8c832d98a0eccaa
ttony62 <1988anton62@gmail.com>
2024-09-27 21:45:40 +0300

    added Routes

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,12 +2,16 @@ import HomeScreen from "./HomeScreen/HomeScreen";
 import styles from './App.module.css'
 import { Routes, Route } from 'react-router-dom'
 import SearchCity from "./SearchCity/SearchCity";
+import { Settings } from "./Settings/Settings";
 
 const App = () => {
 
   return(
     <div className={styles.app}>
-        <HomeScreen />
+      <Routes>
+        <Route path={'/'} element={<HomeScreen />} /> 
+        <Route path={'/settings'} element={<Settings />} />
+      </Routes>
     </div>
     
   )
