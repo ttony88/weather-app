a4d26d1abf0116c503cd04939815b696c22e8af0
ttony62 <1988anton62@gmail.com>
2024-10-06 20:49:30 +0300

    added mui components and icon in Header

--- src/App/HomeScreen/Header/Header.jsx
+++ src/App/HomeScreen/Header/Header.jsx
@@ -1,10 +1,11 @@
 import styles from './Header.module.css'
 import { useNavigate } from 'react-router-dom'
-import searchIcon from '../../../img/search.svg'
 import settingsIcon from '../../../img/settings.svg'
 import SearchCity from './SearchCity/SearchCity'
+import { IconButton } from '@mui/material'
+import SettingsIcon from '@mui/icons-material/Settings'
 
-const Header = ({ cityName, changingCoordinates }) => {
+const Header = ({ cityName, changingCoordinates, isSearch, setIsSearch }) => {
 
     const navigane = useNavigate()
 
@@ -12,19 +13,22 @@ const Header = ({ cityName, changingCoordinates }) => {
         <header className={styles.header}>
             <div className={styles.container}>
                 <div className={styles.row}>
-                    <div className={styles.buttonСhoiceCity}
+                <IconButton variant="contained" 
+                            size="large"
+                            onClick={() => navigane('/settings')} 
+                            className={styles.buttonСhoiceCity} >
+                    <SettingsIcon fontSize="large" sx={{color: '#6865a5'}} />
+                </IconButton>
+                    {/* <div className={styles.buttonСhoiceCity}
                             onClick={() => navigane('/settings')}>
                         <img className={styles.settingsImg} src={settingsIcon} alt="settings" />
-                    </div>
-                    <p className={styles.city}>
+                    </div> */}
+                    {!isSearch && <p className={styles.city}>
                         {cityName}
-                    </p>
-                    <SearchCity changingCoordinates={changingCoordinates} />
-                    {/* <SearchCity /> */}
-                    {/*<button className={styles.buttonReboot}
-                            onClick={() => navigane('moscow', { replace: false })}>
-                        <img className={styles.searchImg} src={searchIcon} alt="search" />
-                    </button>*/}
+                    </p>}
+                    <SearchCity changingCoordinates={changingCoordinates}
+                                isSearch={isSearch}
+                                setIsSearch={setIsSearch} />
                 </div>
             </div>
         </header>
