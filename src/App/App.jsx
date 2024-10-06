a0ae48ec1742bf230e5f6c2f38d7ab8185d31977
ttony62 <1988anton62@gmail.com>
2024-10-06 20:46:51 +0300

    fixed App

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,7 +1,6 @@
 import HomeScreen from "./HomeScreen/HomeScreen";
 import styles from './App.module.css'
 import { Routes, Route } from 'react-router-dom'
-import SearchCity from "./SearchCity/SearchCity";
 import { Settings } from "./Settings/Settings";
 
 const App = () => {
